cfc64587a6935304327c4d0e00cdbf4fd247e08b
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-09-14 16:09:39 -0300

    feat: add navbar-fixed and floating, 3d pin and hero-parallax components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,137 @@
+"use client";
+
+import { NavbarFixed } from "@/components/navbar-fixed";
+import { NavbarFloating } from "@/components/navbar-floating";
+import { PinContainer } from "@/components/ui/3d-pin";
+import { HeroParallax } from "@/components/ui/hero-parallax";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="w-screen h-screen flex items-center justify-center">
-      <h1 className="p-8 rounded shadow text-3xl">
-        Hi, this is my langind page!
-      </h1>
+    <div className="overflow-x-hidden">
+      {NavbarFixed()}
+      {NavbarFloating()}
+      <div className="h-screen w-screen flex items-center justify-center">
+        <PinContainer
+          title="github.com/joaodepaulojp"
+          href="https://github.com/joaodepaulojp"
+        >
+          <div className="flex basis-full flex-col p-4 tracking-tight text-slate-100/50 sm:basis-1/2 w-[20rem] h-[20rem] ">
+            <h3 className="max-w-xs !pb-2 !m-0 font-bold  text-base text-slate-100">
+              Veja meu GitHub
+            </h3>
+            <div className="text-base !m-0 !p-0 font-normal">
+              <span className="text-slate-500 ">
+                Aqui você verá todos meus repositórios!
+              </span>
+            </div>
+            <div className="flex flex-1 w-full rounded-lg mt-4 bg-gradient-to-br from-violet-500 via-purple-500 to-blue-500">
+              <Image src="/readme-github.png" alt="card-github" fill />
+            </div>
+          </div>
+        </PinContainer>
+      </div>
+      <div>
+        <HeroParallax products={products} />
+      </div>
+      <div className="flex flex-col text-9xl items-center h-screen w-screen">
+        Testando
+      </div>
     </div>
   );
 }
+export const products = [
+  {
+    title: "Moonbeam",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+  },
+  {
+    title: "Cursor",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+  },
+  {
+    title: "Rogue",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+  },
+
+  {
+    title: "Editorially",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+  },
+  {
+    title: "Editrix AI",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+  },
+  {
+    title: "Pixel Perfect",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+  },
+
+  {
+    title: "Algochurn",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+  },
+  {
+    title: "Aceternity UI",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+  },
+  {
+    title: "Tailwind Master Kit",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+  },
+  {
+    title: "SmartBridge",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+  },
+  {
+    title: "Renderwork Studio",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/renderwork.png",
+  },
+
+  {
+    title: "Creme Digital",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+  },
+  {
+    title: "Golden Bells Academy",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+  },
+  {
+    title: "Invoker Labs",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+  },
+  {
+    title: "E Free Invoice",
+    link: "#",
+    thumbnail:
+      "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+  },
+];
