8b0938731405f89af48da52caf474bbca8a98b26
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-10-04 21:36:39 -0300

    feat: add menu component and fix change color open menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,9 @@
-"use client";
-
-import StaggeredMenu from "@/components/StaggeredMenu";
-
-const menuItems = [
-  { label: "Home", ariaLabel: "Go to home page", link: "/" },
-  { label: "About", ariaLabel: "Learn about us", link: "/about" },
-  { label: "Services", ariaLabel: "View our services", link: "/services" },
-  { label: "Contact", ariaLabel: "Get in touch", link: "/contact" },
-];
-
-const socialItems = [
-  { label: "Twitter", link: "https://twitter.com" },
-  { label: "GitHub", link: "https://github.com" },
-  { label: "LinkedIn", link: "https://linkedin.com" },
-];
+import Menu from "@/components/menu";
 
 export default function Home() {
   return (
     <div className="overflow-x-hidden">
-      <div style={{ height: "100vh", background: "#1a1a1a" }}>
-        <StaggeredMenu
-          position="right"
-          //FIX: resolver erro de tipagem
-          items={menuItems}
-          socialItems={socialItems}
-          displaySocials={true}
-          displayItemNumbering={true}
-          menuButtonColor="#fff"
-          openMenuButtonColor="#fff"
-          changeMenuColorOnOpen={true}
-          colors={["#B19EEF", "#5227FF"]}
-          logoUrl="/path-to-your-logo.svg"
-          accentColor="#ff6b6b"
-          onMenuOpen={() => console.log("Menu opened")}
-          onMenuClose={() => console.log("Menu closed")}
-          isFixed={true}
-        />
-      </div>
+      <Menu />
     </div>
   );
 }
---
+++ components/menu.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import StaggeredMenu from "@/components/StaggeredMenu";
+
+const menuItems = [
+  { label: "Home", ariaLabel: "Go to home page", link: "/" },
+  { label: "About", ariaLabel: "Learn about us", link: "/about" },
+  { label: "Services", ariaLabel: "View our services", link: "/services" },
+  { label: "Contact", ariaLabel: "Get in touch", link: "/contact" },
+];
+
+const socialItems = [
+  { label: "Twitter", link: "https://twitter.com" },
+  { label: "GitHub", link: "https://github.com" },
+  { label: "LinkedIn", link: "https://linkedin.com" },
+];
+const Menu = () => {
+  return (
+    <div style={{ height: "100vh", background: "#1a1a1a" }}>
+      <StaggeredMenu
+        position="right"
+        //FIX: resolver erro de tipagem
+        items={menuItems}
+        socialItems={socialItems}
+        displaySocials={true}
+        displayItemNumbering={true}
+        menuButtonColor="#fff"
+        openMenuButtonColor="#000"
+        changeMenuColorOnOpen={true}
+        colors={["#B19EEF", "#5227FF"]}
+        logoUrl="/path-to-your-logo.svg"
+        accentColor="#ff6b6b"
+        onMenuOpen={() => console.log("Menu opened")}
+        onMenuClose={() => console.log("Menu closed")}
+        isFixed={true}
+      />
+    </div>
+  );
+};
+
+export default Menu;
