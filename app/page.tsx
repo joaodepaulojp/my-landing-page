a4dba0b4bb5f375adf045fbab3ff484da2468019
JoaoDePaulo <joaodepaulocardoso@edu.unifor.br>
2025-10-17 18:19:35 -0300

    feat: add button component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
 "use client";
 
 import StacksLogo from "@/components/stacksLogo";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="overflow-hidden relative">
+      <div className="h-screen w-screen flex items-center p-4 justify-center flex-col">
+        <h1 className="text-3xl text-center">
+          <em>
+            <span className="text-red-700">{"<"}</span>
+            {"Hello World! "}
+            <span className="text-red-700">{"/>"}</span> <br />
+            <span className="text-xl">
+              Bem-Vindo ao meu <span className="text-red-700">portfólio</span>!
+            </span>
+          </em>
+        </h1>
+        <Button className="mt-4">Ver Projetos</Button>
+      </div>
       <StacksLogo />
     </div>
   );
